f40352ce7ffc03999fd0c3c826887ed123b9738b
WildAlex37 <126484026+WildAlex37@users.noreply.github.com>
2023-05-09 11:55:29 +0200

    Add files via upload

---
+++ exo.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+st.title('Hello Wilders, welcome to my application!')
+
+st.write("I enjoy to discover stremalit possibilities")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df = pd.read_csv(link)
+
+# Here we use "magic commands":
+df
+
+st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+
+
+viz_correlation = sns.heatmap(df.corr(), 
+center=0,
+cmap = sns.color_palette("vlag", as_cmap=True))
+
+st.pyplot(viz_correlation.figure)
+
+
+st.title("'Salut, ceci est la suite de l'exercice")
+
+name = st.text_input("Please give me your name :")
+name_length = len(name)
+st.write("Your name has ",name_length,"characters")
